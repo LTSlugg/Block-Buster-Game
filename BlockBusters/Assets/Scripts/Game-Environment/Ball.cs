c08b994c77b22f6909468d31b5d4ff4651497951
Luis Troncoso <LTSlugg@users.noreply.github.com>
2020-11-02 19:22:13 -0500

    Milestone 1
    Working Player Control and Ball is in Alpha Stages

---
+++ BlockBusters/Assets/Scripts/Game-Environment/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * The Ball script, handles the balls interactions with the paddles and blocks as well as any additional logic to prevent bugs and issues.
+ */
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody2D _rgbd2;
+    [SerializeField] float clampRange = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rgbd2 = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ClampMoveVelocity();
+    }
+
+
+    private void ClampMoveVelocity()
+    {
+        _rgbd2.velocity = Vector3.ClampMagnitude(_rgbd2.velocity, clampRange);
+    }
+}
---
+++ BlockBusters/Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    Rigidbody2D _rgbd2;
+    [SerializeField] int speed = 8;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       _rgbd2 = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        InputChecker();
+    }
+
+    private void InputChecker()
+    {
+        _rgbd2.velocity = new Vector2(speed * Input.GetAxisRaw("Horizontal"), 0);
+    }
+}
